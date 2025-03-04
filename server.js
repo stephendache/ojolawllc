92e41dfd3c4b8ce4cf0e549252886837872a355b
Stephendache <paulstephenedache@gmail.com>
2025-03-03 21:14:41 -0500

    modified the server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'Welcome to Ojo Law LLC!\n',
+    var message = 'Welcome to The The Ojo Law Center, LLC!\n',
         version = 'Running Entirely on NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
