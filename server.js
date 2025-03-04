fc42ab17867ea58ed1028da56405b78137eb6243
Stephendache <paulstephenedache@gmail.com>
2025-03-03 20:59:06 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'Welcome to Ojo Law LLC!\n',
+        version = 'Running Entirely on NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
